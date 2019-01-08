47caec6a3981f9b5d64436aed39a143bfb7b5a99
matthewru <matthewru2005@gmail.com>
2019-01-07 21:45:41 -0600

    ASCL Contest 2 Skeleton

---
+++ ACSL/SampleProblems/Contest_2/clsCheckers.py
@@ -0,0 +1,16 @@
+class Checkers:
+
+    def __init__(self, location):
+        self.location = location
+
+    def jump(self, opRow, opColumn):
+
+        return
+
+    def canJump(self, opRow, opColumn):
+
+        return True
+
+    def isKing(self):
+
+        return True
---
+++ ACSL/SampleProblems/Contest_2/clsLocation.py
@@ -0,0 +1,5 @@
+class Location:
+
+    def __init__(self, row, column):
+        self.row = row
+        self.column = column
