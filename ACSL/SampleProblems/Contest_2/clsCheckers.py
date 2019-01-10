b5fa132677659f45fe54a2772901511439d74710
matthewru <matthewru2005@gmail.com>
2019-01-09 20:15:04 -0600

    Implemented Jump function and tests

--- ACSL/SampleProblems/Contest_2/clsCheckers.py
+++ ACSL/SampleProblems/Contest_2/clsCheckers.py
@@ -3,14 +3,39 @@ class Checkers:
     def __init__(self, location):
         self.location = location
 
-    def jump(self, opRow, opColumn):
-
-        return
-
-    def canJump(self, opRow, opColumn):
-
-        return True
+    def jump(self, opLocation):
+        if self.canJump(opLocation):
+            if not self.isKing():
+                if opLocation.column < self.location.column:
+                    self.location.column = opLocation.column - 1
+                    self.location.row = opLocation.row + 1
+                elif opLocation.column > self.location.column:
+                    self.location.column = opLocation.column + 1
+                    self.location.row = opLocation.row + 1
+            else:
+                if opLocation.row < self.location.row:
+                    self.location.row = opLocation.row - 1
+                    if opLocation.column < self.location.column:
+                        self.location.column = opLocation.column - 1
+                    elif opLocation.column > self.location.column:
+                        self.location.column = opLocation.column + 1
+                elif opLocation.row > self.location.row:
+                    self.location.row = opLocation.row + 1
+                    if opLocation.column < self.location.column:
+                        self.location.column = opLocation.column - 1
+                    elif opLocation.column > self.location.column:
+                        self.location.column = opLocation.column + 1
+        else:
+            return
 
     def isKing(self):
+        return (self.location.row == 8)
 
-        return True
+    def canJump(self, opLocation):
+        if self.isKing():
+            return (opLocation.row == self.location.row + 1 or self.location.row - 1) and (opLocation.column == self.location.column + 1 or opLocation.column == self.location.column - 1) and (opLocation.row != 1 and opLocation.row != 8) and (opLocation.column != 1 and opLocation.column != 8)
+        else:
+            return (opLocation.row == self.location.row + 1) and (opLocation.column == self.location.column + 1 or opLocation.column == self.location.column - 1) and (opLocation.row != 1 and opLocation.row != 8) and (opLocation.column != 1 and opLocation.column != 8)
+
+
+
