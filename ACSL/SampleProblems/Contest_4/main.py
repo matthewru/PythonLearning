09ac21b7427c3ec82a5315d107797079e51fae3b
matthewru <matthewru2005@gmail.com>
2019-01-27 17:41:50 -0600

    Finished Contest 4

--- ACSL/SampleProblems/Contest_4/main.py
+++ ACSL/SampleProblems/Contest_4/main.py
@@ -1,6 +1,8 @@
-#create EllipseForm obj and CircleForm obj
-#if ellipse.isEllipse:
-#   ellipse.ellipse functions
-#elif circle.isCircle:
-#   circle.circle functions
-#print output
+from clsEllipse import Ellipse
+
+for count in range(0, 8):
+    nums = input("What are the numbers? ")
+    numsList = nums.split(", ")
+    ellipse = Ellipse(int(numsList[0]), int(numsList[1]), int(numsList[2]), int(numsList[3]), int(numsList[4]), int(numsList[5]))
+    origX, origY = ellipse.getCenter()
+    print("%s, (%d, %d), %d" % ("Circle" if ellipse.isCircle() else "Ellipse", origX, origY, ellipse.getMajorAxis() if ellipse.isEllipse() else int(ellipse.getMajorAxis()/2)))
