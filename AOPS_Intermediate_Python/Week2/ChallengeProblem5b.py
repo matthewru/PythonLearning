c71c2213293f66cafa983d3b7a444fa665c9fccf
matthewru <matthewru2005@gmail.com>
2019-03-26 20:40:08 -0500

    Finished working out and updating challenge problem 5b

--- AOPS_Intermediate_Python/Week2/ChallengeProblem5b.py
+++ AOPS_Intermediate_Python/Week2/ChallengeProblem5b.py
@@ -1,69 +1,28 @@
 def moneyList(coinList, maxAmount):
-    CoinNum = [0]
+    CoinNum = [0] #list of how many coins it takes to make the looped through amount
 
-    for amount in range(1, maxAmount + 1):
-        leastCoins = maxAmount
-        for coin in coinList:
-            if coin <= amount:
-                neededCoins = 1 + CoinNum[amount - coin]
+    for amount in range(1, maxAmount + 1): #loop through each amount 1 to maxAmount + 1
+        leastCoins = maxAmount #worst case scenario
+        for coin in coinList: #loop through all the coins in coinList
+            if coin <= amount: #see which coins can help make up amount
+                neededCoins = 1 + CoinNum[amount - coin] #to show how many coins are needed
                 if neededCoins < leastCoins:
-                    leastCoins = neededCoins
-        CoinNum.append(leastCoins)
+                    leastCoins = neededCoins #updates scenario
+        CoinNum.append(leastCoins) #adds it to the list
     return CoinNum
 
-coinList = [1, 10, 25]
-moneyList = moneyList(coinList, 99)
-print(sum(moneyList)/100)
+def find_best_denomination(maxAmount):
+    #maxAmount in case of worst case scenario
+    bestAverage = maxAmount
+
+    for coinOne in range(2, maxAmount): #looping through possible coinOnes and coinTwos for best combination
+        for coinTwo in range(coinOne + 1, maxAmount + 1):
+            coinList = [1, coinOne, coinTwo]
+            newAverage = sum(moneyList(coinList, maxAmount)) / (maxAmount + 1)
+            if newAverage < bestAverage:
+                bestAverage = newAverage
+                (bestcoinOne, bestcoinTwo) = (coinOne, coinTwo)
+    return [1, bestcoinOne, bestcoinTwo]
 
 
-
-
-
-
-
-from itertools import combinations
-
-def coinsCount(value, coins):
-    if value <= 0:
-        return 0 #We can only make change for positive values!
-    else:
-        #one option is to just use pennies
-        smallestChange = value
-        #but we should think about using other coins if they will fit
-        for coin in coins:
-            if coin <= value:
-                #consider making change with one of this coin, and however many other coins we need
-                numCoins = 1 + coinsCount(value - coin, coins)
-                if numCoins < smallestChange:
-                    smallestChange = numCoins #we found a better option!
-    return smallestChange # return the best option
-
-def averageCoins(coins):
-    coinNumber = []
-    for i in range(0, 100):
-        print(i)
-        numCoins = coinsCount(i, coins)
-        coinNumber.append(numCoins)
-        print(coinNumber)
-    averageCoinage = sum(coinNumber) / len(coinNumber)
-    return averageCoinage
-
-def twoNumberCombo():
-    numbers = range(2, 100)
-    items = list(combinations(numbers,2))
-    return items
-
-def betterCoinage():
-    combos = twoNumberCombo()
-    bestAverage = 100
-    for combo in combos:
-        print("combo: %s" % str(combo))
-        print("best average: %s" % str(bestAverage))
-        average = averageCoins([1] + list(combo))
-        if average < bestAverage:
-            bestAverage = average
-    return bestAverage
-
-
-#print(averageCoins([1, 10, 25]))
-print(betterCoinage())
+print(find_best_denomination(99))
